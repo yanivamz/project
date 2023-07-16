e203fe71611644b53e3a4baecdd7c4da84c8a43a
Yaniv Amzaleg <yanivamz@gmail.com>
2023-07-17 00:15:49 +0300

    test usage of env varible

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+const text = import.meta.env.VITE_NISIM;
 
 function App() {
   return (
     <>
       <h1>yaniv project a</h1>
+      <h2>{text}</h2>
     
     </>
   )
