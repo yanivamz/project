e670c482eabeef0a3f6776b5e33ec2784f90cf27
Yaniv Amzaleg <yanivamz@gmail.com>
2023-07-23 12:57:00 +0300

    add fetch for test server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 const text = import.meta.env.VITE_NISIM;
 const apiHost = import.meta.env.VITE_API_HOST;
 
-console.log (apiHost);
+fetch(`${apiHost}/test`)
+  .then(res => res.text)
+  .then(data => console.log(data));
 
 function App() {
   return (
     <>
       <h1>yaniv project a</h1>
       <h2>{text}</h2>
-    
+
     </>
   )
 }
