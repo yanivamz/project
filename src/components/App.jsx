16e995f989e51b19eb9156d44c1f3408280aebae
Yaniv Amzaleg <yanivamz@gmail.com>
2023-08-06 12:21:55 +0300

    add css file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 const text = import.meta.env.VITE_NISIM;
 const apiHost = import.meta.env.VITE_API_HOST;
 import yamit from './images/yamit.jpeg'
+import "./style.css"
 
 fetch(`${apiHost}/test`)
   .then(res => res.text())
@@ -10,10 +11,12 @@ fetch(`${apiHost}/test`)
 function App() {
   return (
     <>
+      <div className='box'>
+        <img width='300rem' src={yamit} alt="yamit" />
+        <h1>My Beautiful Wife</h1>
+        <h2>YAMIT</h2>
+      </div>
 
-      <img width='300rem' src={yamit} alt="yamit" />
-      <h1>My Beautiful Wife</h1>
-      <h2>YAMIT</h2>
 
     </>
   )
