39053b16ccc73a1eee2390017c35b31d72e1d912
Yaniv Amzaleg <yanivamz@gmail.com>
2023-08-06 12:04:57 +0300

    updat img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const text = import.meta.env.VITE_NISIM;
 const apiHost = import.meta.env.VITE_API_HOST;
-
+import yamit from './images/yamit.jpeg'
 
 fetch(`${apiHost}/test`)
   .then(res => res.text())
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
 
-      <img width='300rem' src='./src/components/images/yamit.jpeg' alt="yamit" />
+      <img width='300rem' src={yamit} alt="yamit" />
       <h1>My Beautiful Wife</h1>
       <h2>YAMIT</h2>
 
