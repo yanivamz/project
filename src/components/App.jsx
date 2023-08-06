4063a1ba09296796778acf63596caec26c1b5834
Yaniv Amzaleg <yanivamz@gmail.com>
2023-08-06 11:25:42 +0300

    update image url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 const text = import.meta.env.VITE_NISIM;
 const apiHost = import.meta.env.VITE_API_HOST;
 
+
 fetch(`${apiHost}/test`)
   .then(res => res.text())
   .then(data => console.log(data));
@@ -10,9 +11,9 @@ function App() {
   return (
     <>
 
-      <img src="images/yamit.jpeg" alt="yamit" />
+      <img width='300rem' src='./src/components/images/yamit.jpeg' alt="yamit" />
       <h1>My Beautiful Wife</h1>
-      <h2>{text}</h2>
+      <h2>YAMIT</h2>
 
     </>
   )
