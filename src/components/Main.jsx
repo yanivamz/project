75fb5c8fd78206b2fdd78f94d23dc9746e01382f
Yaniv Amzaleg <yanivamz@gmail.com>
2023-08-10 22:00:05 +0300

    fix Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,20 @@
-const text = import.meta.env.VITE_NISIM;
+import React, { useState } from 'react';
 import Catalog from './Catalog.jsx';
-const apiHost = import.meta.env.VITE_API_HOST;
-
-function getProductList() {
-    return fetch(`${apiHost}/products`)
-        .then(res => res.text())
-        .then(data => JSON.parse(data));
-}
-
-const productListData = await getProductList();
 
 function Main() {
-    return <main>
-        <Catalog productList={productListData}/>
-    </main>;
+  const [productListData, setProductListData] = useState([]);
+
+  (async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_HOST}/products`);
+    const data = await response.json();
+    setProductListData(data);
+  })();
+
+  return (
+    <main>
+      <Catalog productList={productListData} />
+    </main>
+  );
 }
 
 export default Main;
